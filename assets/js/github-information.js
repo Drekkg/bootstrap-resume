bd5906499b87d182dda866a6997b7575c920335a
drekkg <rizzagum@gmail.com>
2024-06-07 07:10:51 +0200

    FEAT adds github repo data

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -15,7 +15,28 @@ function userInformationHTML(user) {
       </div>`;
 }
 
+function repoInformationHTML(repos) {
+  if (repos.length === 0) {
+    return `<div class="clearfix repo-list">No Repos!</div>`;
+  }
+  let listItemsHTML = repos.map((repo) => {
+    return `<li>
+    <a href="${repo.html_url}" target=_blank">${repo.name}</a>
+    </li>`;
+  });
+  return `<div class="clearfix repo-list">
+  <p>
+  <strong>Repo List:</strong>
+  </p>
+  <ul>
+  ${listItemsHTML.join("\n")}
+  </ul>
+  </div>`;
+}
+
 function fetchGitHubInformation() {
+  $("#gh-user-data").html("");
+  $("#gh-repo-data").html("");
   let username = $("#gh-username").val();
   if (!username) {
     $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
@@ -24,13 +45,18 @@ function fetchGitHubInformation() {
   $("#gh-user-data").html(`<div id="#loader">
     <img src="assets/styles/loader.gif" alt="Loading...."/></div>`);
 
-  $.when($.getJSON(`https://api.github.com/users/${username}`)).then(
-    function (response) {
-      let userData = response;
+  $.when(
+    $.getJSON(`https://api.github.com/users/${username}`),
+    $.getJSON(`https://api.github.com/users/${username}/repos`)
+  ).then(
+    function (firstResponse, secondResponse) {
+      let userData = firstResponse[0];
+      let repoData = secondResponse[0];
       $("#gh-user-data").html(userInformationHTML(userData));
+      $("#gh-repo-data").html(repoInformationHTML(repoData));
     },
     function (errorResponse) {
-      if (errorResponse.status === 403) {
+      if (errorResponse.status === 404) {
         $("#gh-user-data").html(`<h2>User not found ${username}</h2>`);
       } else {
         console.log(errorResponse);
@@ -39,3 +65,4 @@ function fetchGitHubInformation() {
     }
   );
 }
+$(document).ready(fetchGitHubInformation);
